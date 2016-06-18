418d2c3cdd5db3be983bc6b46b2494f6fd858433
SaiSantoshSb <sai.santosh@flipkart.com>
2016-06-18 16:27:25 +0530

    Code Review Changes

--- Project0/src/main/java/com/example/saisantosh/project0/MainActivity.java
+++ Project0/src/main/java/com/example/saisantosh/project0/MainActivity.java
@@ -3,38 +3,18 @@ package com.example.saisantosh.project0;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity {
 
-    Button popularMovies,
-            stockHawk,
-            buildItBigger,
-            appMaterial,
-            goUbiquitous,
-            capstone;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        popularMovies = (Button) findViewById(R.id.movies);
-        stockHawk = (Button) findViewById(R.id.stock_hawk);
-        buildItBigger = (Button) findViewById(R.id.build_it_bigger);
-        appMaterial = (Button) findViewById(R.id.app_material);
-        goUbiquitous = (Button) findViewById(R.id.ubiquitous);
-        capstone = (Button) findViewById(R.id.capstone);
-        popularMovies.setOnClickListener(this);
-        stockHawk.setOnClickListener(this);
-        buildItBigger.setOnClickListener(this);
-        appMaterial.setOnClickListener(this);
-        goUbiquitous.setOnClickListener(this);
-        capstone.setOnClickListener(this);
     }
 
-    @Override
-    public void onClick(View v) {
+    public void showToast(View v) {
         switch (v.getId()) {
             case R.id.movies:
                 Toast.makeText(getApplicationContext(), "This Button will launch my Popular Movies app!", Toast.LENGTH_SHORT).show();
